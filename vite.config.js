8c89d5998f2518cce4495669b9b484d198c59383
Jitse Dekeyser <jitse.dekeyser@student.howest.be>
2024-03-13 02:22:18 +0100

    finishing details

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  site: "https://jitsedekeyser.be",
+  base: "/web4/react-artistique",
   plugins: [react()],
 })
