a96d7292ccaaf7c73ea65171aab1e9f2705f4c18
Jitse Dekeyser <jitse.dekeyser@student.howest.be>
2024-10-08 14:51:35 +0200

    website url changed

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   site: "https://jitsedekeyser.be",
-  base: "/web4/react-artistique",
+  base: "/devine/react-artistique",
   plugins: [react()],
 })
